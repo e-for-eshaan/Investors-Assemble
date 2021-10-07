be91deeb606e57d30c24d0d48f2290a6f60ca038
Eshaan Yadav <ESHAANYADAV24@GMAIL.COM>
2021-10-07 21:16:44 +0530

    added cards, homepage expanded

---
+++ frontend/src/components/homepage/Card.js
@@ -0,0 +1,25 @@
+import classes from "./Card.module.css";
+
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div className={classes.card}>
+      <div className={classes.circle}></div>
+      <div className={classes.text}>
+          <h3>{props.heading}</h3>
+        <p>
+          {props.comment}
+          </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
+Card.defaultProps = {    
+  heading: "Something",    
+  comment: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid cum voluptatem hic ea incidunt facilis omnis, eligendi itaque at et",    
+  photo: "120"
+}
--- frontend/src/components/homepage/Homepage2.js
+++ frontend/src/components/homepage/Homepage2.js
@@ -1,10 +1,31 @@
-import classes from './Homepage2.module.css' 
-const Homepage2 = () => {
-    return (
-        <div className = {classes.container}>
-            
-        </div>
-    )
-}
+import classes from "./Homepage2.module.css";
+import React from "react";
+import Card from "./Card";
+import { Fade } from "react-awesome-reveal";
 
-export default Homepage2
+
+const Homepage2 = () => {
+  return (
+    <div className={classes.container}>
+      <Fade triggerOnce={true} direction="up">
+      <h1 className={classes.features}>What We Provide</h1>
+      </Fade>
+      <div className={classes.wrap}>
+        <Fade triggerOnce={true} >
+        <Card imgsrc="xyz" text="" head=""/>
+        </Fade>
+        <Fade triggerOnce={true} delay={200}>
+        <Card imgsrc="xyz" text="" head=""/>
+        </Fade>
+        <Fade triggerOnce={true} delay={400}>
+        <Card imgsrc="xyz" text="" head=""/>
+        </Fade>
+        <Fade triggerOnce={true} delay={600}>
+        <Card imgsrc="xyz" text="" head=""/>
+        </Fade>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage2;
