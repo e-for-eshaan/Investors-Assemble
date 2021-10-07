b2e3353550b51c69d3ef262c996177b772d1e725
Eshaan Yadav <ESHAANYADAV24@GMAIL.COM>
2021-10-07 19:59:42 +0530

    homepage section added, Navbar added, Login added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Homepage1 from "./components/homepage/Homepage1";
+import Homepage2 from "./components/homepage/Homepage2";
 import Navbar from "./components/homepage/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Homepage1 />
+      <Homepage2 />
     </div>
   );
 }
---
+++ frontend/src/components/homepage/Homepage2.js
@@ -0,0 +1,10 @@
+import classes from './Homepage2.module.css' 
+const Homepage2 = () => {
+    return (
+        <div className = {classes.container}>
+            
+        </div>
+    )
+}
+
+export default Homepage2
