cb454772e0991c01be0b0ebe0ac1051e949a55ad
chiragsingh1 <imchirag.2717@gmail.com>
2021-10-08 20:35:13 +0530

    fetched all posts from API and mapped posts to the feed, installed timeago.js

--- frontend/src/components/feed/Allposts.js
+++ frontend/src/components/feed/Allposts.js
@@ -1,38 +1,37 @@
 import classes from "./Allposts.module.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Newpost from "./Newpost";
 import Makepost from "./Makepost";
 
+import { format } from "timeago.js";
+import axios from "axios";
+
 const Allposts = () => {
+  // state for storing all the posts in DB
+  const [posts, setPosts] = useState([]);
+  // fetching posts from DB
+  const fetchPosts = async () => {
+    const { data } = await axios.get("/api/posts/all");
+    setPosts(data);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className={classes.allposts}>
       <Makepost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      <Newpost />
-      Allposts
+      {posts.length === 0 ? <h4>No Posts😿</h4> : <h4>All Posts🚀</h4>}
+      {posts.length > 0 &&
+        posts.map((post) => (
+          <Newpost
+            key={post._id}
+            userId={post.userId}
+            time={format(post.createdAt)}
+            desc={post.desc}
+            img={post.img}
+          />
+        ))}
     </div>
   );
 };
--- frontend/src/components/feed/Newpost.js
+++ frontend/src/components/feed/Newpost.js
@@ -1,33 +1,49 @@
-import classes from './Newpost.module.css' 
-import React from 'react'
-import dp from '../images/profile.png'
+import classes from "./Newpost.module.css";
+import React, { useEffect, useState } from "react";
+import dp from "../images/profile.png";
+import axios from "axios";
 
 const Newpost = (props) => {
-    return (
-        <div className = {classes.newpost}>
-            <div className = {classes.head}>
-            <img src={props.dp} alt="404" />
-            <h2>{props.name}</h2>
-            <div className = {classes.time}>
-            <h6>{props.date}</h6>
-            <h6>{props.time}</h6>
-            </div>
-            </div>
-            <div className = {classes.text}>
-                <img src={props.image} alt="" className = {classes.image}/>
-                {props.text}
-            </div>
+  // state for storing user details of each post
+  const [user, setUser] = useState({});
+  // fetching user details of each post from API using props.userId
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/api/users/${props.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [props.userId]);
+  //   console.log(user);
+  return (
+    <div className={classes.newpost}>
+      <div className={classes.head}>
+        <img
+          src={user.avatar === "" ? props.dp : user.avatar}
+          alt="404_user_img"
+        />
+        <h2>{user.name}</h2>
+        <div className={classes.time}>
+          <h6>{props.time}</h6>
         </div>
-    )
-}
+      </div>
+      <div className={classes.text}>
+        {props.img !== "" && (
+          <img src={props.img} alt="404_post_img" className={classes.image} />
+        )}
+        {props.desc}
+      </div>
+    </div>
+  );
+};
 
-Newpost.defaultProps = {    
-    name: "Default Name",    
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores modi aliquid possimus explicabo atque illum ullam nisi maiores voluptas, similique illo, natus ratione veritatis enim voluptatum adipisci quisquam ea temporibus numquam rem id necessitatibus quasi nulla. Hic praesentium ipsum perferendis, numquam veritatis sit, maiores fuga repellendus tempora accusantium saepe ab.",
-    date: "24th July, 2019",
-    time: "7:30pm",
-    image: "https://picsum.photos/536/354",
-    dp: dp,
-  }
+Newpost.defaultProps = {
+  name: "Default Name",
+  text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores modi aliquid possimus explicabo atque illum ullam nisi maiores voluptas, similique illo, natus ratione veritatis enim voluptatum adipisci quisquam ea temporibus numquam rem id necessitatibus quasi nulla. Hic praesentium ipsum perferendis, numquam veritatis sit, maiores fuga repellendus tempora accusantium saepe ab.",
+  date: "24th July, 2019",
+  time: "7:30pm",
+  image: "https://picsum.photos/536/354",
+  dp: dp,
+};
 
-export default Newpost
+export default Newpost;
