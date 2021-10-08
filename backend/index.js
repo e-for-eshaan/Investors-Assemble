577aab95a461384eb8c5922f328daebbb511d416
chiragsingh1 <imchirag.2717@gmail.com>
2021-10-08 16:22:27 +0530

    mongodb connected using mongoose

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,19 @@ const dotenv = require("dotenv");
 
 // app
 const app = express();
+//middlewares
+dotenv.config();
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("--DB Connected.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend Server is running on Port ${process.env.PORT || 5000}.`);
