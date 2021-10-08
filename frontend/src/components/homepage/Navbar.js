ba6ac14457010d74a4b36e1f85d99f053654283f
chiragsingh1 <imchirag.2717@gmail.com>
2021-10-08 19:54:59 +0530

    installed axios, created Register page and registered new user, edited Navbar

--- frontend/src/components/homepage/Navbar.js
+++ frontend/src/components/homepage/Navbar.js
@@ -1,15 +1,30 @@
-import classes from "./Navbar.module.css"
-import {Link} from 'react-router-dom'
-import logo from "../images/logo.png"
+import classes from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 const Navbar = (props) => {
   return (
-    <nav className={classes.nav}  style={{position:props.pos, borderBottomLeftRadius:props.round,borderBottomRightRadius:props.round}}>
-      <img src={logo} alt="" className = {classes.logo}/>
+    <nav
+      className={classes.nav}
+      style={{
+        position: props.pos,
+        borderBottomLeftRadius: props.round,
+        borderBottomRightRadius: props.round,
+      }}
+    >
+      <img src={logo} alt="" className={classes.logo} />
       <ul className={classes.list}>
-        <Link to='/' className={classes.listitem}><li className={classes.listitem}>ITEM 1</li></Link>
-        <Link to='/posts/all' className={classes.listitem}><li className={classes.listitem}>ITEM 2</li></Link>
-        <Link to='/user/profile' className={classes.listitem}><li className={classes.listitem}>ITEM 3</li></Link>
-        <Link to='/' className={classes.listitem}><li className={classes.listitem}>ITEM 4</li></Link>
+        <Link to="/" className={classes.listitem}>
+          <li className={classes.listitem}>Home</li>
+        </Link>
+        <Link to="/feed" className={classes.listitem}>
+          <li className={classes.listitem}>Feed</li>
+        </Link>
+        <Link to="/user/profile" className={classes.listitem}>
+          <li className={classes.listitem}>Profile</li>
+        </Link>
+        <Link to="/register" className={classes.listitem}>
+          <li className={classes.listitem}>Register</li>
+        </Link>
       </ul>
     </nav>
   );
